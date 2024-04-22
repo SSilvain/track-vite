313ee396c02b8110985d436e1e26bcca3809a45d
Silvain <silver383@gmail.com>
2024-04-22 17:31:57 +0300

    brands

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './index.scss'
 //MARK: bodyLock
 export let bodyLockHold = false
 export const bodyLockToggle = (delay = 5) => {
-	0
 	if (document.documentElement.classList.contains('lock')) {
 		bodyUnlock(delay)
 	} else {
@@ -12,42 +11,42 @@ export const bodyLockToggle = (delay = 5) => {
 }
 
 export const bodyLock = (delay) => {
-	let body = document.querySelector("body")
-	if (!bodyLockHold) {
-		//For lp
-		// let lock_padding = document.querySelectorAll("[data-lp]")
-		// for (let index = 0; index < lock_padding.length; index++) {
-		// 	const el = lock_padding[index]
-		// 	el.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px'
-		// }
-		body.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px'
-		document.documentElement.classList.add("lock")
+	if (bodyLockHold) return
 
-		bodyLockHold = true
-		setTimeout(function () {
-			bodyLockHold = false
-		}, delay);
-	}
+	let body = document.querySelector("body")
+	//For lp
+	// let lock_padding = document.querySelectorAll("[data-lp]")
+	// for (let index = 0; index < lock_padding.length; index++) {
+	// 	const el = lock_padding[index]
+	// 	el.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px'
+	// }
+	body.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px'
+	document.documentElement.classList.add("lock")
+
+	bodyLockHold = true
+	setTimeout(function () {
+		bodyLockHold = false
+	}, delay);
 }
 
 export const bodyUnlock = (delay) => {
+	if (bodyLockHold) return
+
 	let body = document.querySelector("body")
-	if (!bodyLockHold) {
-		// let lock_padding = document.querySelectorAll("[data-lp]")
-		setTimeout(() => {
-			//For lp
-			// for (let index = 0; index < lock_padding.length; index++) {
-			// 	const el = lock_padding[index]
-			// 	el.style.paddingRight = '0px'
-			// }
-			body.style.paddingRight = '0px'
-			document.documentElement.classList.remove("lock")
-		}, delay)
-		bodyLockHold = true
-		setTimeout(function () {
-			bodyLockHold = false
-		}, delay)
-	}
+	// let lock_padding = document.querySelectorAll("[data-lp]")
+	setTimeout(() => {
+		//For lp
+		// for (let index = 0; index < lock_padding.length; index++) {
+		// 	const el = lock_padding[index]
+		// 	el.style.paddingRight = '0px'
+		// }
+		body.style.paddingRight = '0px'
+		document.documentElement.classList.remove("lock")
+	}, delay)
+	bodyLockHold = true
+	setTimeout(function () {
+		bodyLockHold = false
+	}, delay)
 }
 
 //MARK: menu
@@ -58,15 +57,14 @@ const menu =
 		if (!isButtonClass) throw 'the buttonClass is missing'
 
 		document.addEventListener('click', function (e) {
-			if (!bodyLockHold) {
+			if (bodyLockHold) return
 
-				const isClickOnButton = e.target.closest(buttonClass)
-				if (isClickOnButton) {
+			const isClickOnButton = e.target.closest(buttonClass)
+			if (isClickOnButton) {
 
-					bodyLockToggle()
-					document.documentElement.classList.toggle(openClassForToggle)
+				bodyLockToggle()
+				document.documentElement.classList.toggle(openClassForToggle)
 
-				}
 			}
 		})
 
@@ -84,3 +82,4 @@ const menu =
 
 const menuInit = menu({ buttonClass: '.icon-menu', openClassForToggle: 'menu-open' })
 
+alert('location')
